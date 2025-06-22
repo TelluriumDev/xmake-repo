fef5f8a12343a127e6a4d4a05963440ef315d06b
xianyubb <2149656630@qq.com>
2025-06-22 12:13:06 +0800

    Create xmake.lua

---
+++ packages/n/nodejs/xmake.lua
@@ -0,0 +1,12 @@
+package("nodejs")
+    add_urls("https://github.com/TelluriumDev/node/releases/download/v$(version)/sdk.zip")
+
+    add_versions("24.2.0", "cbd40deacfb83cec62e78ec53f5800e0b52150aa6560e2a963eabe017524f06e")
+
+    add_includedirs("include/")
+    add_includedirs("include/v8/")
+    add_includedirs("include/uv/")
+
+    on_install(function (package)
+        os.cp("*", package:installdir())
+    end)
